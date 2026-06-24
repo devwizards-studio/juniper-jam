c35e0da8f9e99080d6be6cc3dfc2930093e0a0e8
Dani Munteanu <daniel.munteanu@student.upt.ro>
2026-06-24 18:45:44 +0300

    Added ShurikenSpawner and CombatStats autoload
    The shurikens spawn well in the Player scene, but not in the main scene. I don't know why. Will try again later

---
+++ Scripts/combat/shuriken.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+var move_direction: Vector2
+var start_position: Vector2
+var move_speed: float = 5.0
+var acceleration: float = 0.0
+var radius: float = 2.0
+
+var horizontal_angle: float = 0
+var horizontal_angle_spacing: float = 0
+
+var wiggle_speed: float = 0
+var horizontal_wiggle_size: float = 0
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	var horizontal_wiggle_modifier = horizontal_wiggle_size * sin(wiggle_speed * Time.get_ticks_msec())
+
+	var shuriken_dir_x_position = start_position.x + cos(((horizontal_angle + horizontal_angle_spacing + horizontal_wiggle_modifier) * PI) / 180.0) * radius
+	var shuriken_dir_y_position = start_position.y + sin(((horizontal_angle + horizontal_angle_spacing + horizontal_wiggle_modifier) * PI) / 180.0) * radius
+
+	var new_position = Vector2(shuriken_dir_x_position, shuriken_dir_y_position)
+	move_direction = (new_position - start_position).normalized()
+
+	global_position += move_direction * move_speed * delta
+	if start_position.distance_to(global_position) >= radius:
+		queue_free()
+
+	move_speed += acceleration;
---
+++ Scripts/combat/shuriken_spawner.gd
@@ -0,0 +1,52 @@
+extends Node2D
+
+const SHURIKEN = preload("res://scenes/combat/shuriken.tscn")
+
+@onready var timer: Timer = $Timer
+
+@export var radius: float = 500.0
+@export var horizontal_angle: float = 0
+@export var horizontal_angle_step: float = 10
+
+@export var horizontal_wiggle_size: float = 0
+@export var wiggle_speed: float = 0
+
+@export var shuriken_speed: float = 200
+@export var acceleration: float = 0
+	
+func _on_timer_timeout() -> void:
+	print("fired 1 ring")
+	spawn_shurikens()
+	
+func spawn_shurikens():
+	var start_point = global_position
+	var horizontal_angle_spacing = 360.0 / CombatStats.number_of_shurikens;
+	var horizontal_wiggle_modifier = horizontal_wiggle_size * sin(wiggle_speed * Time.get_ticks_msec())
+	
+	for i in range(0, CombatStats.number_of_shurikens):
+		var shuriken_dir_x_position = start_point.x + cos(((horizontal_angle + i * horizontal_angle_spacing + horizontal_wiggle_modifier) * PI) / 180.0) * radius
+		var shuriken_dir_y_position = start_point.y + sin(((horizontal_angle + i * horizontal_angle_spacing + horizontal_wiggle_modifier) * PI) / 180.0) * radius
+		
+		var new_position_vector = Vector2(shuriken_dir_x_position, shuriken_dir_y_position)
+		var shuriken_direction = (new_position_vector - start_point).normalized()
+		
+		var new_shuriken = SHURIKEN.instantiate()
+		new_shuriken.global_position = start_point
+		new_shuriken.start_position = start_point
+		new_shuriken.radius = radius
+		
+		new_shuriken.move_speed = shuriken_speed
+		new_shuriken.acceleration = acceleration
+		new_shuriken.move_direction = shuriken_direction
+
+		new_shuriken.horizontal_angle = horizontal_angle
+		new_shuriken.horizontal_angle_spacing = i * horizontal_angle_spacing
+		new_shuriken.wiggle_speed = wiggle_speed
+		new_shuriken.horizontal_wiggle_size = horizontal_wiggle_size
+		
+		add_child(new_shuriken)
+	
+		if horizontal_angle >= 360.0:
+			horizontal_angle -= 360.0
+			
+	horizontal_angle += horizontal_angle_step
